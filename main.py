0bc73a479ffba1b8c0d91b91ae420dea4de699e8
Denis Drozdov <denis.drozdov@playtech.com>
2024-01-13 14:01:30 +0200

    print only sync bytes

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 import serial
 from time import sleep
 
-# ser = serial.Serial("/dev/ttyS0", 115200)  # Open port with baud rate
-ser = serial.Serial("/dev/ttyS0", 420000)  # Open port with baud rate
+ser = serial.Serial("/dev/ttyS0", 115200)  # Open port with baud rate
 while True:
     received_data = ser.read()  # read serial port
     sleep(0.03)
@@ -10,4 +9,13 @@ while True:
     received_data += ser.read(data_left)
     # print (received_data)                   #print received data
     print(len(received_data))
-    ser.write(received_data)  # transmit data serially
+    # ser.write(received_data)  # transmit data serially
+    # 0xC8
+    for b in received_data:
+        if b == 0xC8:
+            print(b)
+
+    print(" ---- ")
+
+
+
