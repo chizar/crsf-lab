aecf8149f0171b1ee25493a55ae7566f3edfbd1e
Denis Drozdov <denis.drozdov@playtech.com>
2024-01-14 13:59:38 +0200

    size configurable

--- brigde.py
+++ brigde.py
@@ -6,8 +6,12 @@ baud = 425000  #420000?
 if len(sys.argv) > 1:
     baud = sys.argv[1]
 
+size = 100
+if len(sys.argv) > 2:
+    size = sys.argv[2]
+
 with Serial("/dev/ttyS0", baud, timeout=1) as ser:
     while True:
-        values = ser.read(100)
+        values = ser.read(size)
         ser.write(values)
         print(len(values))
