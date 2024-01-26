3e680332b3aab1caa656d57bfc6458b1910766a6
Denis Drozdov <denis.drozdov@playtech.com>
2024-01-26 17:06:20 +0200

    smart args logger

--- logger.py
+++ logger.py
@@ -1,11 +1,21 @@
+import sys
+import argparse
+
 from serial import Serial
+
+parser = argparse.ArgumentParser()
+parser.add_argument('-b', '--baud', default=1000, type=int)
+parser.add_argument('-t', '--timeout', default=1, type=int)
+parser.add_argument('-ss', '--serialsize', default=10, type=int)
+
+args = parser.parse_args()
 
 SYNC_BYTE = 0xC8
 
-with Serial("/dev/ttyS0", 1000, timeout=10) as ser:
+with Serial("/dev/ttyS0", args.baud, timeout=args.timeout) as ser:
     inputByteArray = bytearray()
     while True:
-        values = ser.read(100)
+        values = ser.read(args.serialsize)
         for byte in values:
             if byte == SYNC_BYTE:
                 print("GOT SYNC BYTE")
