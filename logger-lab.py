57d7e487036c219cfa459159f6aaebe25ab7dbb0
Denis Drozdov <denis.drozdov@playtech.com>
2024-01-27 18:39:55 +0200

    serial as a thread

--- logger-lab.py
+++ logger-lab.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
 import argparse
+from threading import Thread
+import time
 
 from serial import Serial
 
@@ -16,30 +18,43 @@ SYNC_BYTE = 0xC8
 FRAME_SIZE = 26
 
 iteration = 0
-last_pos = 0
 
 valuesRest = b""
 
-with Serial(args.port, args.baud, timeout=args.timeout) as ser:
-    inputByteArray = bytearray()
-    total = 0
-    while True:
-        iteration += 1
-        values = valuesRest + ser.read(args.serialsize)
+last_frame = bytearray()
+total_frames = 0
+last_read_size = 0
+last_actual_frame_size = 0
 
-        size = len(values)
-        pos = 0
-        for byte in values:
-            if byte == SYNC_BYTE:
 
-                frame = values[pos:pos + FRAME_SIZE]
-                if len(frame) < FRAME_SIZE:
-                    valuesRest = frame
-                    continue
+def monitor_serial():
+    global args, iteration, total_frames, last_read_size, last_actual_frame_size, last_frame
 
-                frame_size = len(frame)
-                last_pos = pos
-                total += 1
-                print(f'iteration {iteration:05d}; sync {total} found on {pos}, frame size {frame_size}, total size {size}')
-                print(frame)
-            pos += 1
+    with Serial(args.port, args.baud, timeout=args.timeout) as ser:
+
+        while True:
+            iteration += 1
+            values = valuesRest + ser.read(args.serialsize)
+
+            last_read_size = len(values)
+            pos = 0
+            for byte in values:
+                if byte == SYNC_BYTE:
+
+                    frame = values[pos:pos + FRAME_SIZE]
+                    if len(frame) < FRAME_SIZE:
+                        valuesRest = frame
+                        continue
+
+                    last_actual_frame_size = len(frame)
+                    total_frames += 1
+                    # print(f'iteration {iteration:05d}; sync {total} found on {pos}, frame size {frame_size}, total size {size}')
+                    # print(frame)
+                    last_frame = frame
+                pos += 1
+
+
+serial_thread = Thread(target=monitor_serial, name="serial")
+serial_thread.start()
+
+time.sleep(10_000)
