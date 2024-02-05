0b6a63bf05a7d7cc2d9b48664542d33ab08eaf30
Denis Drozdov <denis.drozdov@playtech.com>
2024-02-05 18:21:08 +0200

    show degrees

--- logger-altitude-lab.py
+++ logger-altitude-lab.py
@@ -2,6 +2,7 @@
 
 import argparse
 import time
+import math
 from threading import Thread
 
 from asciimatics.screen import Screen
@@ -80,9 +81,9 @@ def dashboard(screen):
             if actual_crc != crc:
                 crc_errors += 1
 
-            screen.print_at(f'PITCH:{altitude.pitch:05d} '
-                            f'ROLL:{altitude.roll:05d} '
-                            f'YAW:{altitude.yaw:05d} '
+            screen.print_at(f'PITCH:{math.degrees(altitude.pitch):05d} '
+                            f'ROLL:{math.degrees(altitude.roll):05d} '
+                            f'YAW:{math.degrees(altitude.yaw):05d} '
                             , 0, dashboard_lines.DASHBOARD_CHANNELS_LINE)
 
 
